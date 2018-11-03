75a27e3f8a708e1beadd86cb9c9488fa46d438cb
legendhl <legendhl@gmail.com>
2018-11-03 23:13:32 +0800

    add free move to nfa

--- chapter3/nfa.rb
+++ chapter3/nfa.rb
@@ -26,17 +26,26 @@ class NFARuleBook < Struct.new(:rules)
     def rule_for(state, character)
         rules.select { |rule| rule.applies_to?(state, character) }
     end
+
+    def follow_free_moves(states)
+        more_states = next_states(states, nil)
+        if more_states.subset?(states)
+            states
+        else
+            states | follow_free_moves(more_states)
+        end
+    end
 end
 
 rulebook = NFARuleBook.new([
-    FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
-    FARule.new(2, 'a', 3), FARule.new(2, 'b', 3),
-    FARule.new(3, 'a', 4), FARule.new(3, 'b', 4),
+    FARule.new(1, nil, 2), FARule.new(1, nil, 4),
+    FARule.new(2, 'a', 3), FARule.new(3, 'a', 2),
+    FARule.new(4, 'a', 5), FARule.new(5, 'a', 6),
+    FARule.new(6, 'a', 4),
 ])
 
-# puts rulebook.next_states(Set[1], 'b') # 1,2
-# puts rulebook.next_states(Set[1,2], 'a') # 1,3
-# puts rulebook.next_states(Set[1,3], 'b') # 1,2,4
+# puts rulebook.next_states(Set[1], nil) # 2,4
+# puts rulebook.follow_free_moves(Set[1]) # 1,2,4
 
 class NFA < Struct.new(:current_states, :accept_states, :rulebook)
     def accepting?
@@ -50,6 +59,10 @@ class NFA < Struct.new(:current_states, :accept_states, :rulebook)
     def read_string(string)
         string.each_char { |c| read_character(c) }
     end
+
+    def current_states
+        rulebook.follow_free_moves(super)
+    end
 end
 
 # nfa = NFA.new(Set[1], [3], rulebook)
@@ -60,9 +73,9 @@ end
 # nfa.read_string('abbbb')
 # puts nfa.accepting?
 
-class NFADesign < Struct.new(:start_states, :accept_states, :rulebook)
+class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
     def to_nfa
-        NFA.new(start_states, accept_states, rulebook)
+        NFA.new(Set[start_state], accept_states, rulebook)
     end
 
     def accepts?(string)
@@ -73,6 +86,8 @@ class NFADesign < Struct.new(:start_states, :accept_states, :rulebook)
     end
 end
 
-nfaDesign = NFADesign.new(Set[1], [3], rulebook)
-puts nfaDesign.accepts?('ab')
-puts nfaDesign.accepts?('abbbb')
+nfa_design = NFADesign.new(1, [2,4], rulebook)
+puts nfa_design.accepts?('aa') # true
+puts nfa_design.accepts?('aaa') # true
+puts nfa_design.accepts?('aaaaa') # false
+puts nfa_design.accepts?('aaaaaa') # true
