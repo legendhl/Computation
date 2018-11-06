79b100742d5bb8eaccb8b1678f9889c0c62f17c1
legendhl <legendhl@gmail.com>
2018-11-06 22:51:44 +0800

    minimize a dfa

--- chapter3/nfa2dfa.rb
+++ chapter3/nfa2dfa.rb
@@ -28,11 +28,62 @@ class NFASimulation < Struct.new(:nfa_design)
     end
 end
 
-# rulebook = NFARuleBook.new([
-#     FARule.new(1, 'a', 1), FARule.new(1, 'a', 2),
-#     FARule.new(1, nil, 2), FARule.new(2, 'b', 3),
-#     FARule.new(3, 'b', 1), FARule.new(3, nil, 2),
+class DFAMinimum < Struct.new(:dfa_design)
+    def reverse_dfa(dfa_design)
+        rules = dfa_design.rulebook.rules.map { |rule| FARule.new(rule.next_state, rule.character, rule.state) }
+        accept_states = [dfa_design.start_state]
+        if dfa_design.accept_states.size > 1
+            start_state = Object.new
+            rules += dfa_design.accept_states.map { |state| FARule.new(start_state, nil, state) }
+        else
+            start_state = dfa_design.accept_states[0]
+        end
+        rulebook = NFARuleBook.new(rules)
+        NFADesign.new(start_state, accept_states, rulebook)
+    end
+
+    def minimize(dfa_design)
+        nfa_design = reverse_dfa(dfa_design)
+        dfa_design = NFASimulation.new(nfa_design).to_dfa_design
+        nfa_design = reverse_dfa(dfa_design)
+        dfa_design = NFASimulation.new(nfa_design).to_dfa_design
+        puts dfa_design
+        puts
+        normalize(dfa_design)
+    end
+
+    def normalize(dfa_design)
+        states = Set[]
+        hash = Hash.new
+        n = 1
+        states.add?(dfa_design.start_state)
+        dfa_design.rulebook.rules.map do |rule|
+            states.add?(rule.next_state)
+            states.add?(rule.state)
+        end
+        states.each do |state|
+            hash[state] = n
+            n = n + 1
+        end
+        states = states + dfa_design.accept_states.to_set
+        start_state = hash[dfa_design.start_state]
+        accept_states = dfa_design.accept_states.map { |state| hash[state] }
+        rules = dfa_design.rulebook.rules.map { |rule| FARule.new(hash[rule.state], rule.character, hash[rule.next_state]) }
+        rulebook = DFARuleBook.new(rules)
+        DFADesign.new(start_state, accept_states, rulebook)
+    end
+end
+
+# rulebook = DFARuleBook.new([
+#     FARule.new(1, 'a', 2), FARule.new(1, 'b', 1),
+#     FARule.new(2, 'a', 2), FARule.new(2, 'b', 3),
+#     FARule.new(3, 'a', 3), FARule.new(3, 'b', 3),
 # ])
+
+# dfa_design = DFADesign.new(1, [2,3], rulebook)
+# dfa_minimizer = DFAMinimum.new(dfa_design)
+# mini_dfa_design = dfa_minimizer.minimize(dfa_design)
+# puts mini_dfa_design
 
 # puts rulebook.alphabet
 # puts rulebook.next_states(Set[1], nil) # 2,4
@@ -59,8 +110,3 @@ end
 # puts simulation.discover_states_and_rules(Set[start_state])
 # puts nfa_design.to_nfa(Set[1,2]).accepting?
 # puts nfa_design.to_nfa(Set[2,3]).accepting?
-
-# dfa_desigin = simulation.to_dfa_design
-# puts dfa_desigin.accepts?('aaa')
-# puts dfa_desigin.accepts?('aab')
-# puts dfa_desigin.accepts?('bbbabb')
